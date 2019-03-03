7dd5f5734f6ddaa290a850f25ea63e123f2a080d
Davi Gadelha Leitão <dgleitao.contratos@gmail.com>
2019-03-03 18:54:47 -0300

    Adding the Spring Boot project
    We added a backend folder to the project
    with a simple GetMapping request to test
    the application's RESTful API.

---
+++ backend/src/main/java/com/davileitao/webservices/getyourtasksdone/GetYourTasksDoneApplication.java
@@ -0,0 +1,13 @@
+package com.davileitao.webservices.getyourtasksdone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetYourTasksDoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetYourTasksDoneApplication.class, args);
+	}
+
+}
