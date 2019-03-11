dbb80ba855e719e6600bd89264a3f5e661f16d71
Davi Gadelha Leitão <dgleitao.contratos@gmail.com>
2019-03-10 22:41:15 -0300

    updating a task and routing after an update

--- frontend/src/app/service/data/task-service.ts
+++ frontend/src/app/service/data/task-service.ts
@@ -1,35 +1,41 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { HardcodedAuthenticationService } from '../hardcoded-authentication.service';
-import { Task } from 'src/app/models/Task';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { HardcodedAuthenticationService } from "../hardcoded-authentication.service";
+import { Task } from "src/app/models/Task";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TaskService {
-
   constructor(
     private http: HttpClient,
     private authService: HardcodedAuthenticationService
-  ) { }
+  ) {}
 
   retrieveAllTasks() {
     const username = this.authService.getLoggedUser();
-    return this.http.get<Task[]>(`http://localhost:8080/users/${username}/tasks`);
+    return this.http.get<Task[]>(
+      `http://localhost:8080/users/${username}/tasks`
+    );
   }
 
   retrieveTask(username: string, id: number) {
-    return this.http.get<Task>(`http://localhost:8080/users/${username}/tasks/${id}`);
+    return this.http.get<Task>(
+      `http://localhost:8080/users/${username}/tasks/${id}`
+    );
   }
 
-  editAction(id: number) {
-    // const username = this.authService.getLoggedUser();
-    // return this.http.get
-
+  updateTask(username: string, id: number, task: Task) {
+    return this.http.put(
+      `http://localhost:8080/users/${username}/tasks/${id}`,
+      task
+    );
   }
 
   deleteAction(id: number) {
     const username = this.authService.getLoggedUser();
-    return this.http.delete(`http://localhost:8080/users/${username}/tasks/${id}`)
+    return this.http.delete(
+      `http://localhost:8080/users/${username}/tasks/${id}`
+    );
   }
 }
--- frontend/src/app/task/task.component.ts
+++ frontend/src/app/task/task.component.ts
@@ -1,38 +1,38 @@
-import { Component, OnInit, Injectable } from '@angular/core';
-import { TaskService } from 'src/app/service/data/task-service';
-import { ActivatedRoute } from '@angular/router';
-import { Task } from '../models/Task';
+import { Component, OnInit, Injectable } from "@angular/core";
+import { TaskService } from "src/app/service/data/task-service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Task } from "../models/Task";
 
 @Component({
-  selector: 'app-task',
-  templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+  selector: "app-task",
+  templateUrl: "./task.component.html",
+  styleUrls: ["./task.component.css"]
 })
 export class TaskComponent implements OnInit {
-
   id: number;
   task: Task;
 
   constructor(
     private taskService: TaskService,
-    private route: ActivatedRoute
-  ) { }
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.id = this.route.snapshot.params['id'];
-    this.task = new Task(1, '', false, new Date());
-    this.taskService.retrieveTask('davi', this.id).subscribe(
-
-      data => {
-        console.log(data);
-        this.task = data;
-      }
-
-    );
+    this.id = this.route.snapshot.params["id"];
+    const username = sessionStorage.getItem("authenticatedUser");
+    this.task = new Task(1, "", false, new Date());
+    this.taskService.retrieveTask(username, this.id).subscribe(data => {
+      this.task = data;
+    });
   }
 
   saveTask() {
-
+    const username = sessionStorage.getItem("authenticatedUser");
+    this.taskService
+      .updateTask(username, this.id, this.task)
+      .subscribe(data => {
+        this.router.navigate(["tasks"]);
+      });
   }
-
 }
